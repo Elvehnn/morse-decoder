23b2dd6188d57436445a3d9268ba8a5af3fd6e39
Elena Shashina <elvehnn@yandex.ru>
2020-10-25 21:50:16 +0300

    feat: implement function decodes Morse code and returns a string

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const exprArray = expr.match(/(\d{10})|(\*{10})/g);
+    let letterArray = [];
+    let morseArray = []; morseArrayDecoded = [];
+    let strDecoded = '';
+    // function howTo(m){
+      // return MORSE_TABLE[m];  // Возвращает значение ключа `m` в объекте `MORSE_TABLE`
+    // };
+    for (let i = 0; i < exprArray.length; i++ ) {
+      letterArray[i] = exprArray[i].match(/(\d{2})|(\*{2})/g);
+      morseArray[i] = '';
+      for (let j = 0; j < letterArray[i].length; j++ ) {
+        if (letterArray[i][j] === '**') {
+          morseArray[i] = '*';
+          break;
+        } else {
+            switch (letterArray[i][j]) {
+                case '10': {
+                  morseArray[i] += '.';
+                  break;
+                };
+                case '11': {
+                  morseArray[i] += '-';
+                  break;
+                };
+            };
+        };
+      };
+    };
+    for (let i = 0; i < morseArray.length; i++ ) { 
+      if (morseArray[i] === '*') {
+        morseArrayDecoded[i] = ' ';
+      } else {
+        morseArrayDecoded[i] = morseArray[i].replace(/.+/g, m => MORSE_TABLE[m]);
+      };
+    };    
+    strDecoded = morseArrayDecoded.join('');
+    return (strDecoded); 
 }
 
 module.exports = {
